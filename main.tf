4bdde49766c224d5763375dc2b1859f7b1f67aa8
Evandro Martinelli <martinelli.evandro@gmail.com>
2022-02-16 12:50:11 -0300

    TF

--- main.tf
+++ main.tf
@@ -3,10 +3,9 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.65"
+      version = "=2.91.0"
     }
   }
-  required_version = ">= 0.14.9"
 }
 provider "azurerm" {
   features {}
@@ -14,15 +13,16 @@ provider "azurerm" {
 
 # Create the resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "myResourceGroup-${random_integer.ri.result}"
+  name     = "rg-dev-rifa"
   location = "eastus"
 }
 # Create the Linux App Service Plan
 resource "azurerm_app_service_plan" "appserviceplan" {
-  name                = "webapp-asp-${random_integer.ri.result}"
+  name                = "appserviceplan-dev-rifa"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   kind                = "linux"
+  reserved            = true
   sku {
     tier = "Free"
     size = "F1"
@@ -30,7 +30,7 @@ resource "azurerm_app_service_plan" "appserviceplan" {
 }
 # Create the web app, pass in the App Service Plan ID, and deploy code from a public GitHub repo
 resource "azurerm_app_service" "webapp" {
-  name                = "webapp-${random_integer.ri.result}"
+  name                = "appservice-dev-rifa"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
@@ -45,7 +45,7 @@ resource "azurerm_app_service" "webapp" {
 }
 # Create the Azure Container Registry
 resource "azurerm_container_registry" "acr" {
-  name                = "acrname"
+  name                = "acrdevrifa"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
